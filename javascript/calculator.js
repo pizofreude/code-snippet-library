fc3c2ec6f59c0f42db0d4de4738084b9bc4ea146
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-06-10 20:14:37 +0800

    javascript/calculator.js: Add calculator.

---
+++ javascript/calculator.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b;
+}
+
+// Example usage
+console.log(add(5, 3));
+
+// This is JavaScript code that defines a function that adds two numbers and returns the result.
+
+// The `function` keyword is used to define a new function.
+
+// The name of the function is `add`.
+
+// The `a` and `b` parameters are used to pass in two numbers.
+
+// The `return` statement returns the sum of the two numbers.
+
+// The `console.log` statement logs the result of calling the `add` function with arguments 5 and 3 to the console.
