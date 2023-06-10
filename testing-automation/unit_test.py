3ffc26e3282d5cd9900cbb61664eef4426f8b54f
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-06-10 20:21:32 +0800

    testing-automation/unit_test.py: Add unit test.

---
+++ testing-automation/unit_test.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+# Unit test
+assert add(2, 3) == 5
+
+# This is Python code that defines a function that adds two numbers and returns the result.
+
+# The `def` keyword is used to define a new function.
+
+# The name of the function is `add`.
+
+# The `a` and `b` parameters are used to pass in two numbers.
+
+# The `return` statement returns the sum of the two numbers.
+
+# The `assert` statement checks that calling the `add` function with arguments 2 and 3 returns 5. If the assertion fails, an error will be raised.
+
+# This is an example of a unit test for the `add` function.
