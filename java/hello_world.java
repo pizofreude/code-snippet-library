24060c5cc2d4e612b208da2519c92a927c39044c
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-06-10 20:12:39 +0800

    java/hello_world.java: Print string in java.

---
+++ java/hello_world.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+// This is Java code that defines a class with a main method that prints "Hello, world!" to the console.
+
+// The `public` keyword indicates that the class is visible and accessible from outside the package.
+
+// The `class` keyword is used to define a new class.
+
+// The name of the class is `HelloWorld`.
+
+// The `main` method is the entry point of the program.
+
+// It is declared as `public static void`, which means it can be called from outside the class without creating an instance of the class.
+
+// The `String[] args` parameter is an array of command-line arguments.
+
+// The `System.out.println` statement prints "Hello, world!" to the console.
